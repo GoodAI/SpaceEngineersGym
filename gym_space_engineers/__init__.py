55efbd26de529994508a73aca5f423bcc450221a
Antonin Raffin <antonin.raffin@ensta.org>
2021-07-21 00:23:30 +0200

    Add test env

--- gym_space_engineers/__init__.py
+++ gym_space_engineers/__init__.py
@@ -23,6 +23,17 @@ register(
 )
 
 register(
+    id="SE-WalkingTest-v1",
+    entry_point="gym_space_engineers.envs:WalkingRobotIKEnv",
+    # max_episode_steps=200,  # around 20s of interaction
+    kwargs={
+        "control_frequency": 10,  # 10Hz
+        "symmetric_control": True,
+        "add_end_effector_velocity": True,
+    },
+)
+
+register(
     id="SE-Symmetric-v1",
     entry_point="gym_space_engineers.envs:WalkingRobotIKEnv",
     max_episode_steps=200,  # around 20s of interaction
